e67fd495a2b999500c1ff456b6b44c501c468e1d
Eric Khoury <khoury@live.se>
2018-03-30 21:08:55 +0200

    Adding dummy data

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, FlatList } from 'react-native';
+import { List, ListItem  } from "react-native-elements";
+import Moment from 'moment';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor() {
+    super()
+
+    var dt = '2018-06-18';
+    var date = Moment(dt).format('D/M')
+
+    this.state = {
+      data: [
+        { title: 'Big Ben', date: date, price: 0 },
+        { title: 'Andra lång Comedy', date: date, price: 0  },
+        { title: 'Harrys standup', date: date, price: 0 },
+        { title: 'Garva Göteborg', date: date, price: 0 },
+        { title: 'Brooklyn Haha', date: date, price: 0 },
+        { title: 'Flygarns haga comedy', date: date, price: 0 },
+        { title: 'El skratto', date: date, price: 0 },
+        { title: 'Björns skrattfest', date: date, price: 0 },
+      ]
+    }
+  }
+
   render() {
+    Moment.locale('sv');
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0, marginTop: 100, padding: 30 }}>
+        <FlatList
+          data={ this.state.data }
+          renderItem={({item}) => <Text>{item.title} | {item.date} | {item.price} kr</Text>}
+        />
+      </List>
     );
   }
 }
@@ -20,4 +45,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    marginTop: 150,
+    fontSize: 30
+  },
+  list: {
+    marginTop: 20
+  }
 });
